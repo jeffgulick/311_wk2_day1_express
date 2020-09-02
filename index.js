6e4c1c02126d057868d0968df9fe271b6b2149e9
JEFFGULICK <jeffgulick@gmail.com>
2020-09-02 16:18:16 -0500

    all routes working

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 
 const express = require('express')
-const app = express()
+const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 4000
 
-const { users } = require('./state')
+const { users } = require('./state');//saves to users variable
+let counter = users.length;
+app.use(bodyParser.json());//need to post new users?
 
 /* BEGIN - create routes here */
 app.get('/users',(req, res) => {
   res.json(users);
 })
 
+//any user
 app.get('/users/:id', (req, res) => {
-  res.json(users.filter(user => user._id == (req.params.id)))
+  res.json(users.filter(user => user._id == (req.params.id)));
 });
 
-app.post('/users', (req, res) => )
+//post new user
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+  newUser._id = counter + 1;
+  users.push(newUser);
+  res.json(users);
+  counter++;
+})
 
+//put
+app.put('/users/:id', (req, res) => {
+  users.forEach(user => {
+    if(user._id == (req.params.id)){
+      user.name = req.body.name;
+      user.occupation = req.body.occupation;
+      user.avatar = req.body.avatar;
+      res.json(user)
+    }
+  })
+})
+
+//delete
+app.delete('/users/:id', (req, res) => {
+
+  users.forEach(user => {
+    if(user._id == (req.params.id)){
+      user.isActive = false;
+      // res.json(user);
+      res.send(`User ${user._id} has been removed`);
+    }
+  })
+})
 /* END - create routes here */
 
 app.listen(port, () => 
